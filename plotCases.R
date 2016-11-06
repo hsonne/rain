851acb8dd67de5de9b5be0ec5ae9e8f3e73e7b5a
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2016-11-06 10:19:52 +0100

    this belongs to the last commit

--- plotCases.R
+++ plotCases.R
@@ -4,11 +4,17 @@ plotCases <- function
 (
   cases, rainData, diffs = NULL, 
   neighb = NULL, num.neighb = 0, trim = TRUE, to.pdf = TRUE,
+  method = 1, # 1: use all rain data, 2: use only rain data of rain days
   ...
 )
 {
   if (! is.null(diffs)) {
     stopifnot(nrow(cases) == length(diffs))
+  }
+
+  if (method == 2) {
+    rainDays <- unique(cases$day)
+    rainData <- rainData[rainData$day %in% as.character(rainDays), ]
   }
 
   file.pdf <- preparePdfIf(to.pdf)
