7d23d64b58d2873db4e343b1683d136bae0ef3f4
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2016-11-11 07:46:28 +0100

    simplify call of plotRainForValidation()

--- plotCases.R
+++ plotCases.R
@@ -13,21 +13,21 @@ plotCases <- function
   # Filter rain data for days to be considered in the cases
   rainDays <- unique(cases$day)
   rainData <- rainData[rainData$day %in% as.character(rainDays), ]
-
+  
   file.pdf <- callWith(preparePdfIf, args.pdf, to.pdf = to.pdf)
   on.exit(finishAndShowPdfIf(to.pdf, file.pdf))
   
   for (i in seq_len(nrow(cases))) {
     
     case <- cases[i, ]
-
+    
     caseData <- selectCaseData(rainData, case, neighb, trim = trim)
     
     main <- toPlotTitle(case = case, marked = 0, #sum(rdiff$diff)
                         prefix = sprintf("Case %d/%d - ", i, nrow(cases)))
     
     cat("\n***", main, "\n")
-
+    
     plotCase(
       case, caseData, diffinfo = diffs[[i]], main = main
       , ...
@@ -54,18 +54,15 @@ plotCase <- function
   rd <- caseData[, -(2:3)]
   
   if (method == 1) {
+    
     plotRainAtGauge(rd, main = main)
+    
   } else if (method == 2) {
-    plotArgs <- list(
-      rd = rd, main = main
-      #, rdiff = data.frame(decreasingOrder = prp, diff = hts - rdd[prp, gauge])
-      #, label = .barLabels(rd)
+    
+    plotRainForValidation(
+      rd = rd, main = main, barheights = .toBarheights(rd, diffinfo)
       #, ...
     )
-
-    barheights <- .toBarheights(rd, diffinfo)
-    
-    callWith(plotRainForValidation, plotArgs, barheights = barheights)
   }
 }
 
